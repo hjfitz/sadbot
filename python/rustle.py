57cd03069ab0b95d15e6376a1fd16179f823a799
hjpa <spedoid@gmail.com>
2013-08-18 16:11:53 +0100

    rustle fixt

--- python/rustle.py
+++ python/rustle.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
+# That really rustled my jimmies.
 # by andri
 
 from random import choice
 from sys import argv
 
 with open('/usr/share/dict/words') as f:
-    l = f.readlines()
     if len(argv) < 4:
-        verb = choice(l).strip('\n')
-        noun = choice(l).strip('\n')
-        print 'That really %sed my %sies.' %(verb, noun)
+        l = f.readlines()
+        ed = filter(lambda i: i[-3:-1] == 'ed', l)
+        ies = filter(lambda i: i[-4:-1] == 'ies', l)
+        verb = choice(ed).strip('\n')
+        noun = choice(ies).strip('\n')
+        print 'That really %s my %s.' %(verb, noun)
     else:
         verb = argv[2]
         noun = argv[3]
