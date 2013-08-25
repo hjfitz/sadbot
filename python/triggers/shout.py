d4b1f2d38831eae13d880f1616775be6e840f69e
hjpa <spedoid@gmail.com>
2013-08-25 14:31:30 +0100

    fixed repeated strings and memory leak (lel)

---
+++ python/triggers/shout.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python
+# shoutbot handler
+# by Corg-kun
+
+from sys import argv
+from random import choice
+
+uniqlines = set(open('./data/shoutdb').readlines())
+out = open('./data/shoutdb', 'w').writelines(uniqlines)
+
+#concatenate message into a single string.
+message = ' '.join(argv[1:])
+#check if message was shouted and greater than 5 characters in length
+if message.upper() == message and len(message) > 4:
+    #open shout messages db file
+    with open("./data/shoutdb", 'r+') as f:
+        #creat a list of strings of past shouted messages.
+        db = f.read().splitlines()
+        print choice(db)
+        #randomly choose an element from the db to shout back
+        f.write(message + '\n')
+
