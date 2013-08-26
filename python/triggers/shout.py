20bdb0342da1ef2601f2113c750c43ceff06468b
Corg-kun <RMS@gnu.org>
2013-08-26 10:22:00 +0100

    Possible fix for shout module
    Might fix issue where shout module sometimes repeats input.

--- python/triggers/shout.py
+++ python/triggers/shout.py
@@ -7,18 +7,18 @@ from random import choice
 
 #concatenate message into a single string.
 message = ' '.join(argv[1:])
-#check if message was shouted and greater than 5 characters in length
+#check if message was shouted and greater than 5 characters in length.
 if message.upper() == message and len(message) > 4:
     #open shout messages db file
     with open("./data/shoutdb", 'r+') as f:
         #creat a list of strings of past shouted messages.
         db = f.read().splitlines()
-        #randomly choose an element from the db to shout back
+        #randomly choose an element from the db and shout it back.
         out = choice(db)
+        print out
         #check to see if the message already exists in the db, if so, return a line
-        #from the db but do not write the new message it to the db
+        #from the db but do not write the new message it to the db.
         if not message in db:
             f.write(message + '\n')
-        print out
 
 
