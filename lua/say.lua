f92086d4d0c02791ba424bd6293fa516f3ff5117
speccy <shadowzreloaded@gmail.com>
2013-08-26 17:26:06 +0100

    fixed say

--- lua/say.lua
+++ lua/say.lua
@@ -1,1 +1,6 @@
-print(string.format(arg[1]:match( "^%s*(.-)%s*$" )))
+str = arg[1]
+if string.match(arg[1], "[k]sadbot") then
+    print("nise try but na")
+else
+    print(string.format(arg[1]:match( "^%s*(.-)%s*$" )))
+end
