807101b477226989eeadec81848067a8a2f904dd
Andri Soone <crapmuffinz@gmail.com>
2013-08-26 13:50:29 +0300

    Added escape support and comments

--- python/triggers/sed.py
+++ python/triggers/sed.py
@@ -4,27 +4,40 @@
 from sys import argv
 import os
 
+# This script is executed every time a PRIVMSG happens
+# Get nick, channel and PRIVMSG from argv
 nick = '<'+argv[1]+'>'
 channel = argv[2].split(' ')[2]
-sed = ' '.join(argv[3:])
+sed = ' '.join(argv[3:]).strip(' ')
 
+# Replace escaped slashes, because they would interfer with what it's about to do
+# Hopefully nobody will ever say <OMGASLASH>
+sed.replace('\\/','<OMGASLASH>')
+
+# If PRIVMSG starts with s/ and there's another / in there somewhere
 if sed[:2] == "s/" and "/" in sed[2:]:
-    if '\\' in sed:
-        print "sry but na"
-    else:
-        with open("logs/%s" %channel.replace('/','-')) as f:
-            f = f.read().split('\n')[::-1]
-            for i in f:
-                if nick in i:
-                    msg = i[i.find(nick)+len(nick)+1:]
-                    break
-                else:
-                    continue
+    # Open the log for the current channel, should be in the format of "logs/#channelname"
+    # Otherwise change this
+    # Slashes should be replaced with dashes
+    with open("logs/%s" %channel.replace('/','-')) as f:
+        f = f.read().split('\n')[::-1]
+        for i in f:
+            if nick in i:
+                # Finds last occurrence of the nick who wants to sed
+                msg = i[i.find(nick)+len(nick)+1:]
                 break
-        if sed[-2:] == "/g":
-            ye = "g"
-        else:
-            ye = ""
-        i, o = sed.split('/')[1:3]
-        a = os.popen("echo \"%s\" | sed 's/%s/%s/%s'" %(msg, i, o, ye)).read().replace('\n','')
-        print nick, a
+            else: # This is used to break out of 2 loops
+                continue
+            break
+    # Checks if the second last character is a slash
+    if sed[-2] == "/":
+        # The last character is the thing after the slash, for example /g
+        ye = msg[-1]
+    else:
+        ye = ""
+    i, o = sed.split('/')[1:3]
+    # <OMGASLASH> is replaced by an escaped slash once again
+    i, o = i.replace('<OMGASLASH>','\\/'), o.replace('<OMGASLASH>', '\\/')
+    # Uses os.popen to finally execute the command
+    a = os.popen("echo \"%s\" | sed 's/%s/%s/%s'" %(msg, i, o, ye)).read().replace('\n','')
+    print nick, a
