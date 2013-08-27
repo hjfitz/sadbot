24d19ba1b188ec71f1ae153c43d32784a339fcd0
Andri Soone <crapmuffinz@gmail.com>
2013-08-27 17:40:39 +0300

    Update beer.py

--- python/beer.py
+++ python/beer.py
@@ -3,9 +3,12 @@
 # by andri
 from sys import argv
 
+# If there are no arguments (and the argv length is only 2), it uses the nick of the command executor
+# Otherwise gets the nick from arguments
 if len(argv) == 2:
     recipient = argv[1]
 else:
     recipient = ' '.join(argv[2:]).strip(' ')
 
+# [me] is the ACTION keyword for the bot
 print "[me] hands %s a beer." %recipient
