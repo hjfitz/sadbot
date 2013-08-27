01a11073f1d933c9c2725ff46b5448be1919b188
Andri Soone <crapmuffinz@gmail.com>
2013-08-27 16:57:56 +0300

    Update calc.py

--- python/calc.py
+++ python/calc.py
@@ -6,12 +6,19 @@ from sys import argv
 import urllib2
 
 url = "http://www.google.com/ig/calculator?q="
-a = ' '.join(argv[2:])
-b = urllib2.urlopen(url + urllib2.quote(a))
+a = ' '.join(argv[2:]) # This is the input
+b = urllib2.urlopen(url + urllib2.quote(a)) # This is the output of the calculator API
+# Something like this: {lhs: "2 * 2",rhs: "4",error: "",icc: false}
+
+# Sorting the output a bit
 b = b.readlines()[0]
+# Doo doo doo dee dooo
 b = b[b.find('rhs: "')+6:b.find('",e')]
+# Some of this
 b = b.decode('unicode-escape')
+# Some of that
 b = b.replace(u'\xc2\xa0', ',').replace('<sup>', '^(').replace('</sup>', ')').replace('&#215;','x')
+# Boom prints the output if there is any
 if b:
     print b
 else:
