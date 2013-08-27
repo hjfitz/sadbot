6966e9f234febd03b48089e5ee0f4694d602a3fe
Andri Soone <crapmuffinz@gmail.com>
2013-08-27 16:59:34 +0300

    Update lmddgtfy.py

--- python/lmddgtfy.py
+++ python/lmddgtfy.py
@@ -5,6 +5,9 @@ from urllib2 import quote
 from sys import argv
 
 msg = quote(' '.join(argv[1:]))
+# Uses urllib2's quote function to turn the input into a proper URL input
 url = "https://lmddgtfy.net/?q="
 
+# That's pretty much it
+# It doesn't even connect to the web
 print url + msg
