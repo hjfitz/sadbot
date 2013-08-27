21ba4b93738255ae5531a3c8600bbcdc2deaf244
Andri Soone <crapmuffinz@gmail.com>
2013-08-27 16:58:34 +0300

    Update decide.py

--- python/decide.py
+++ python/decide.py
@@ -5,5 +5,7 @@
 from sys import argv
 from random import choice
 
+# Makes a list of all the options that are separated by " or "
 i = ' '.join(argv[2:]).split(' or ')
+# Returns a random option from the list
 print choice(i)
