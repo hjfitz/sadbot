0155ceb8912dbe57bce492465e055ebc20a0ad12
speccy <shadowzreloaded@gmail.com>
2013-08-27 12:39:29 +0100

    fixed define module

--- lua/define.lua
+++ lua/define.lua
@@ -2,15 +2,15 @@ local json = require("json")
 local http = require("socket.http")
 local url = require("socket.url")
 
-query = arg[2]
+query = arg[2]:gsub(" ", "+")
 
 local c = http.request(string.format("http://www.google.com/dictionary/json?callback=a&sl=en&tl=en&q=%s", query))
 local c = string.sub(c, 3,-11)
-print(c)
 local result = json.decode(c) or false      -- json hackery that returns a table using the lastfm json apii
-
-type = string.lower(result.primaries[1].terms[1].labels[1].text)
-
-definition = result.primaries[1].entries[2].terms[1].text
-
-print(query .. " (" .. type .. ") " .. definition)
+if result.primaries then
+    type = string.lower(result.primaries[1].terms[1].labels[1].text)
+    definition = result.primaries[1].entries[2].terms[1].text
+    print(arg[2] .. " [" .. type .. "] " .. definition ".")
+else
+    print("No definitions found")
+end
