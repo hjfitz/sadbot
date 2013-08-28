52a9ab7dca96dab9e22915e07a03d58cf8e71e04
Corg-kun <RMS@gnu.org>
2013-08-28 13:57:44 +0100

    Added mad module
    Would be surprised if it works first time

---
+++ python/mad.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# mad module
+# by Corg-kun
+
+print ("MAD LEVEL|\x036=====CALM===\x0312===MILDLY IRRITATED===\x0311===DISPLEASED===\x039======UPSET=====\x038=====IRATE=====\x037======RAGING======\x034========****ANDRI MAD****===")
