3a53d13b8d3bc52ef5faa43f66df18a6f06d4711
speccy <shadowzreloaded@googlemail.com>
2013-09-02 08:17:17 +0000

    update to mad.py (again)

--- python/mad.py
+++ python/mad.py
@@ -2,4 +2,4 @@
 # mad module
 # by Corg-kun
 
-print ("MAD LEVEL|\x036=====CALM===\x0312===MILDLY IRRITATED===\x0311===DISPLEASED===\x039======UPSET=====\x038=====IRATE=====\x037======RAGING======\x034========****NE RAGE****===")
+print ("MAD LEVEL|\x036=====CALM===\x0312===MILDLY IRRITATED===\x0311===DISPLEASED===\x039======UPSET=====\x038=====IRATE=====\x037======MAD======\x034========****NE RAGE****===")
