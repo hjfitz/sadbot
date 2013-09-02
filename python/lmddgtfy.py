45902231392e813fb7c7aa61c39f985951af7dbd
Andri Soone <crapmuffinz@gmail.com>
2013-09-02 19:29:28 +0300

    Update lmddgtfy.py

--- python/lmddgtfy.py
+++ python/lmddgtfy.py
@@ -2,9 +2,13 @@
 # Let Me DuckDuckGo That For You
 # by andri
 from urllib2 import quote
-from sys import argv
+import sys
 
-msg = quote(' '.join(argv[1:]))
+msg = quote(' '.join(sys.argv[1:]))
+
+# Checks for argument
+if msg.strip(' ') == '':
+    sys.exit('No input')
 # Uses urllib2's quote function to turn the input into a proper URL input
 url = "https://lmddgtfy.net/?q="
 
