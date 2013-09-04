404c2f8956fae4ea0a80d9cde507a565af7f4f1f
speccy <shadowzreloaded@googlemail.com>
2013-09-04 23:53:25 +0000

    wrote flirt module

---
+++ ruby/flirt.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+flirts = [
+"If I could rearrange the alphabet, I would put U and I together.",
+"Are you a parking ticket? Because you've got FINE written all over you.",
+"I've been bad, take me to your room.",
+"I better get out my library card, 'cause I'm checkin' you out.",
+"If I said you had a good body, would you hold it against me?",
+"Nice butt.",
+"That shirt looks good on you, it'd look better on my bedroom floor.",
+"I lost my phone number, can I have yours?",
+"Hi, I'm Mr. Right. Someone said you were looking for me.",
+"You got something on your chest: My eyes.",
+"Your daddy must have been a baker, because you've got a nice set of buns.",
+"I'm not drunk, I'm just intoxicated by you.",
+"If beauty were time, you'd be eternity.",
+"Hey baby, you must be a light switch, cuz every time I see you, you turn me on!",
+"You're so beautiful that you made me forget my pickup line.",
+"Inheriting eighty million bucks doesn't mean much when you have a weak heart.",
+"I may not be a window repairman, but I can still fill your crack in.",
+"You might not be the best looking girl here, but beauty is only a light switch away.",
+"I must be a snowflake, because I've fallen for you.",
+"If you were a steak you would be well done.",
+"If I received a nickel for everytime I saw someone as beautiful as you, I'd have five cents. ",
+"Are those space pants? Because your ass is out of this world!",
+"It’s a good thing I wore my gloves today; otherwise, you’d be too hot to handle."
+]
+
+puts flirts.sample
