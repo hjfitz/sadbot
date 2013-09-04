4cf8db07cc47bdcda31a5b19b749206d275b2b2c
speccy <shadowzreloaded@googlemail.com>
2013-09-04 15:30:25 +0000

    added tell function

---
+++ ruby/triggers/tell.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+dest_nick = ARGV[0]
+arg = ARGV[1]
+chan = ARGV[2].split(" ")[2]
+
+entries = IO.readlines("./data/telldb").uniq
+
+for line in entries
+    if line.split(" ")[0] == chan and line.split(" ")[2] == dest_nick
+
+	message = line.split(' ').drop(3).join(' ')
+	src_nick = line.split(' ')[1] 
+
+	out = "#{dest_nick}: <#{src_nick}> #{message}"
+	
+	entries.delete(line)
+    end
+end
+
+puts out
+
+new = File.open("./data/telldb", "w")
+
+for line in entries
+    new.syswrite(line)
+end
+
+File.close
