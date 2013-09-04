248ea84cdc5f1b167cdecd722c741841f8fd0073
speccy <shadowzreloaded@googlemail.com>
2013-09-04 11:46:27 +0000

    fixed the doge

--- python/doge.py
+++ python/doge.py
@@ -4,5 +4,5 @@ from random import randint
 #picks a random number to be used in the color
 ranColor= randint(1,16)
 #outputs the random color
-print("x\003" + str(ranColor) + "You have been visited by the doge bird of shibe. good luck and prosperity will come to you, but only if you post \"thanks doge\" in this channel.")
+print("\x03" + str(ranColor) + "You have been visited by the doge bird of shibe. good luck and prosperity will come to you, but only if you post \"thanks doge\" in this channel.")
 
