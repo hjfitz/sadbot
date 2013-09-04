41ad430bea4dba13a82296385a38a557bb3259e4
speccy <shadowzreloaded@googlemail.com>
2013-09-04 15:35:35 +0000

    fix

---
+++ ruby/tell.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+chan = ARGV[2].split(" ")[2]
+src_nick = ARGV[0].strip()
+input = ARGV[1].strip()
+
+if not File::exists?("./data/telldb")
+    File.new("./data/telldb", "w")
+end
+
+f = File.open("./data/telldb", "a")
+dest_nick = input.split(' ')[0]
+message = input.split(' ').drop(1).join(' ')
+f.syswrite("#{chan} #{src_nick} #{dest_nick} #{message}\n")
+    
